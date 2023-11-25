db2b6ab9fbf43c7bac47cdf7459d16b9760f14ee
23navi <navisureka23@gmail.com>
2023-11-25 20:00:03 +0530

    using tailwind for css

--- SG/app-router-basics/src/app/page.tsx
+++ SG/app-router-basics/src/app/page.tsx
@@ -1,16 +1,6 @@
-import Image from "next/image";
 import homeImg from "public/home.jpg";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return (
-    <div className="absolute -z-10 inset-0">
-      <Image
-        src={homeImg}
-        alt="home image"
-        fill
-        style={{ objectFit: "cover" }}
-      ></Image>
-      <h1>Home</h1>
-    </div>
-  );
+  return <Hero imgData={homeImg} title="Home" altText="Home page" />;
 }
