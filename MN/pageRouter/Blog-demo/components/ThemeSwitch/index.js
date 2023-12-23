1679220fe7209f30a5a5ade01dbf72fa72adb405
23navi <navisureka23@gmail.com>
2023-12-23 22:39:52 +0530

    button

---
+++ MN/pageRouter/Blog-demo/components/ThemeSwitch/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./styles.module.css";
+
+const ThemeSwitch = () => {
+  return <button className={style.switchButton}>Dark Mode</button>;
+};
+
+export default ThemeSwitch;
--- MN/pageRouter/Blog-demo/pages/index.js
+++ MN/pageRouter/Blog-demo/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import getPosts from "../libs/getPosts";
+import ThemeSwitch from "../components/ThemeSwitch";
 
 export async function getStaticProps() {
   const posts = await getPosts();
@@ -14,7 +15,11 @@ export async function getStaticProps() {
 const index = ({ posts }) => {
   return (
     <main>
-      <h1>Home</h1>
+      <div className="indexDiv">
+        <h1>Home</h1>
+        <ThemeSwitch />
+      </div>
+
       <div>
         <ul>
           {posts.map((post) => (
