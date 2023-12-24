141c90383dfca9a461033f16e14aeaf97d787461
23navi <navisureka23@gmail.com>
2023-12-24 10:16:14 +0530

    nex-shop init

--- MN/pageRouter/next-shop/next-shop-fe/pages/index.js
+++ MN/pageRouter/next-shop/next-shop-fe/pages/index.js
@@ -1,3 +1,14 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1 className="text-orange-950">Helloooo</h1>;
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main className="px-6 py-4">
+        <h1>Next Shop</h1>
+      </main>
+    </>
+  );
 }
