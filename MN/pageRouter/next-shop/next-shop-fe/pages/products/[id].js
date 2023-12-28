2a6158375be2723fa100063b4577da79e4aa9a1f
23navi <navisureka23@gmail.com>
2023-12-28 11:34:17 +0530

    revalidation and backend 500 handle

--- MN/pageRouter/next-shop/next-shop-fe/pages/products/[id].js
+++ MN/pageRouter/next-shop/next-shop-fe/pages/products/[id].js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import axios from "axios";
 
 export const getStaticPaths = async () => {
@@ -8,18 +7,28 @@ export const getStaticPaths = async () => {
     params: { id: product.id.toString() },
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: "blocking" };
 };
 
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  const product = await axios.get(`http://localhost:1337/products/${id}`);
-  return {
-    props: {
-      product: product.data,
-    },
-    revalidate: 30, // 10 seconds
-  };
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const product = await axios.get(`http://localhost:1337/products/${id}`);
+    return {
+      props: {
+        product: product.data,
+      },
+      revalidate: 30, // 10 seconds
+    };
+  } catch (err) {
+    console.log(err.response.status);
+    if (err.response.status === 404) {
+      return {
+        notFound: true,
+      };
+    }
+  }
 };
 
 export default function Product(props) {
---
+++ MN/pageRouter/next-shop/next-shop-fe/pages/products/loading.js
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <p>Loading...</p>
+      {/* You can add a loading spinner or any other UI elements here */}
+    </div>
+  );
+};
+
+export default Loading;
