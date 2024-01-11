d961d4880b1cdffdf44c9554571c95bea48d5647
23navi <navisureka23@gmail.com>
2024-01-11 23:51:17 +0530

    minor changes

--- MN/appRouter/next-review/app/reviews/[slug]/page.tsx
+++ MN/appRouter/next-review/app/reviews/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import Heading from '@/components/Heading';
-import { getReview, getSlugs } from '@/lib/reviews';
+import Heading from "@/components/Heading";
+import { getReview, getSlugs } from "@/lib/reviews";
 
 interface ReviewPageParams {
   slug: string;
@@ -14,16 +14,25 @@ export async function generateStaticParams(): Promise<ReviewPageParams[]> {
   return slugs.map((slug) => ({ slug }));
 }
 
-export default async function ReviewPage({ params: { slug } }: ReviewPageProps) {
+// here props wo ReviewPage will be props={params: {slug: string}}
+export default async function ReviewPage({
+  params: { slug }, // destructure params from props then destructuring slug
+}: ReviewPageProps) {
   const review = await getReview(slug);
+  console.log("This is running");
   return (
     <>
       <Heading>{review.title}</Heading>
       <p className="italic pb-2">{review.date}</p>
-      <img src={review.image} alt=""
-        width="640" height="360" className="mb-2 rounded"
+      <img
+        src={review.image}
+        alt=""
+        width="640"
+        height="360"
+        className="mb-2 rounded"
       />
-      <article dangerouslySetInnerHTML={{ __html: review.body }}
+      <article
+        dangerouslySetInnerHTML={{ __html: review.body }}
         className="max-w-screen-sm prose prose-slate"
       />
     </>
