f2304b308ae656cc5db18c2c1992edff9871a54f
23navi <navisureka23@gmail.com>
2024-11-09 11:01:20 +0530

    edit page init

--- SG/code-snippet/src/app/snippets/[id]/edit/page.tsx
+++ SG/code-snippet/src/app/snippets/[id]/edit/page.tsx
@@ -1,11 +1,22 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
 interface SnippetEditPageProps {
   params: {
     id: string;
   };
 }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
   const id = parseInt(props.params.id);
+
+  const snippet = await db.snippet.findUnique({
+    where: { id }, // We converted string id to int id
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
 
   return <div>Editing snippet with id {id}</div>;
 }
