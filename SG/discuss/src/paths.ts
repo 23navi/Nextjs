9ca64509d9b09f0736fa2c2ab1746bde4b4d3a27
23navi <navisureka23@gmail.com>
2024-11-27 20:37:24 +0530

    path helper

---
+++ SG/discuss/src/paths.ts
@@ -0,0 +1,16 @@
+const paths = {
+  homePage() {
+    return "/";
+  },
+  topicShowPath(slug: string) {
+    return `/topics/${slug}`;
+  },
+  postCreatePath(slug: string) {
+    return `/topics/${slug}/posts/new`;
+  },
+  postShowPath(slug: string, postId: string) {
+    return `/topics/${slug}/posts/${postId}`;
+  },
+};
+
+export default paths;
