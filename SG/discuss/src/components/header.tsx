40dde96d81e1811bc2bb3a62d419f47fedaaa65c
23navi <navisureka23@gmail.com>
2024-11-28 22:18:25 +0530

    header init

---
+++ SG/discuss/src/components/header.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+} from "@nextui-org/react";
+
+import { auth } from "@/auth";
+
